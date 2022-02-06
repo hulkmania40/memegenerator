ae943e0834c999ed4ed1a24b635f9b7036e3f626
Mubashir Ali <ali.mubashir40@gmail.com>
2022-02-06 23:18:40 +0530

    Until 17 - States started

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import memesData from '../data/memesData'
 
 export default function Meme() {
-  
+    
+    const [memeImage, setMemeImage] = useState("")
+
     function getMemeImage(){
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
@@ -15,16 +16,17 @@ export default function Meme() {
         <form>
             <div className='row'>
                 <div className="col-lg-6 form--input">
-                    <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Top Text" />
+                    <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Top Text" />
                 </div>
                 <div className='col-lg-6 form--input'>
-                    <input type="password" className="form-control" placeholder="Bottom Text"/>
+                    <input type="text" className="form-control" placeholder="Bottom Text"/>
                 </div>
             </div>
         </form>
         <div className='row'>
                 <button className="form--btn text-center" onClick={getMemeImage}>Get a new Image <i className='material-icons glyph-top'>face</i></button>
         </div>
+        <img src={memeImage} alt="" className='mx-auto d-block meme--image'/>
     </main>
   );
 }
