01a6853df26b3d1604abe6c63b5251ed47871176
Mubashir Ali <ali.mubashir40@gmail.com>
2022-03-04 14:36:19 +0530

    link form to img caption

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import memesData from '../data/memesData'
+import '../Meme.css'
 
 export default function Meme() {
     
@@ -12,6 +13,15 @@ export default function Meme() {
     })
 
     const [allMemeImage, setAllMemeImage] = useState(memesData)
+
+    function handleChange(event){
+        const {name,value} = event.target
+        setMeme(prevMeme=>({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     function getMemeImage(){
         const memesArray = allMemeImage.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
@@ -24,20 +34,25 @@ export default function Meme() {
 
     return (
     <main>
-        <form>
+        <div className='form'>
             <div className='row'>
                 <div className="col-lg-6 form--input">
-                    <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Top Text" />
+                    <input type="text" className="form-control" placeholder="Top Text" name="topText" value={meme.topText} onChange={handleChange} />
                 </div>
                 <div className='col-lg-6 form--input'>
-                    <input type="text" className="form-control" placeholder="Bottom Text"/>
+                    <input type="text" className="form-control" placeholder="Bottom Text" name="bottomText" value={meme.bottomText} onChange={handleChange} />
                 </div>
             </div>
-        </form>
+        </div>
         <div className='row'>
                 <button className="form--btn text-center" onClick={getMemeImage}>Get a new Image <i className='material-icons glyph-top'>face</i></button>
         </div>
-        <img src={meme.randomImage} alt="" className='mx-auto d-block meme--image'/>
+            {/* <img src={meme.randomImage} alt="" className='mx-auto d-block meme--image'/> */}
+            <div className="meme">
+                <img src={meme.randomImage} className="mx-auto d-block meme--image" alt="" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
     </main>
   );
 }
