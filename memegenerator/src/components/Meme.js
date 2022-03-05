030f353a63cd2d32f11cae3128420c2f4a94ffc2
Mubashir Ali <ali.mubashir40@gmail.com>
2022-03-05 17:28:01 +0530

    default img change

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -6,7 +6,7 @@ export default function Meme() {
     const[meme, setMeme] = useState({
         topText:"",
         bottomText:"",
-        randomImage:"https://i.imgflip.com/30b1gx.jpg"
+        randomImage:"https://i.imgflip.com/1bil.jpg"
     })
 
     const [allMemes, setAllMemes] = useState([])
