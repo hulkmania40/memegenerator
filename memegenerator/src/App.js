cd531d6d076f62a97ac73622b7f55f9914484ea8
Mubashir Ali <ali.mubashir40@gmail.com>
2022-03-05 18:00:06 +0530

    Add google Analytics to track usage

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
+// import ReactGA from 'react-ga';
 
 function App() {
+  // ReactGA.initialize('G-0VWJ1VZZWW');
+  // ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <div className='container-fluid'>
       <Header />
--- memegenerator/src/components/Header.js
+++ memegenerator/src/components/Header.js
@@ -7,9 +7,12 @@ export default function Header() {
         <div className="container">
             <a className="navbar-brand" href="/">
                 <img src={logo} alt="" className='nav--logo'/>
-                <span className='nav--text'>Meme Generator - Beta Version <img src="https://img.icons8.com/external-justicon-lineal-color-justicon/40/000000/external-emoji-emoji-justicon-lineal-color-justicon-4.png"/></span>
+                <span className='nav--text'>Meme Generator </span>
             </a>
-            <h4>Made with <img src="https://img.icons8.com/fluency/40/000000/filled-like.png"/> by Mubashir</h4>
+            <h4>Made with <img src="https://img.icons8.com/fluency/40/000000/filled-like.png" alt="" /> by Mubashir</h4>
+            <span className='nav--text'><strong>Beta version</strong>
+            <img src="https://img.icons8.com/external-justicon-lineal-color-justicon/25/000000/external-emoji-emoji-justicon-lineal-color-justicon-4.png" alt="" /></span>
+            
         </div>
     </nav>
   </div>
